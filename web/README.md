7005965d1d280889ed7827c3ae5706581fe924a1
Jeremy Plsek <jeremyplsek@gmail.com>
2016-02-05 23:19:17 -0500

    add some initial js code and interface test additions

--- web/README.md
+++ web/README.md
@@ -2,7 +2,7 @@
 
 This is the web server for YAP.
 
-## THIS IS CURRENTLY NON-FUNCTIONAL.
+# THIS IS CURRENTLY NON-FUNCTIONAL.
 
 This uses nodejs as a backend.
 
@@ -13,5 +13,5 @@ This uses nodejs as a backend.
 ## Running
 * Run with `./run.sh` or `node server.js`
 
-# Interface
+## Interface
 * [Here](https://realtimeboard.com/app/board/iXjVOf5YT3s=/) is a rough layout of the design for the web and native client. If you would like to access this, please message one of us, as we don't think we can create a public link!
--- web/public/js/client.js
+++ web/public/js/client.js
@@ -1,8 +1,59 @@
 $(function () {
 
-"use strict";
+'use strict';
 
+// server
+var server = {
+    // current server the user is in
+    current: null,
+    // server list
+    list: [],
+
+    // join a server (either via the tab or the join button)
+    join: function (id) {
+
+    },
+
+    initEvents: function () {
+        $(document).on('click', '#server-tabs a', function () {
+            var id = $(this).data().id;
+            server.join(id);
+        });
+    }
+};
+
+// voice channel
+var voice = {
+    // the current voice channel
+    current: null,
+
+    // join a voice channel
+    join: function (id) {
+
+    },
+
+    initEvents: function () {
+        $(document).on('click', '#channel-voice a', function () {
+            var id = $(this).data().id;
+            voice.join(id);
+        });
+    }
+};
+
+// chat channel
 var chat = {
+    // the current chat channel the user is in
+    current: null,
+
+    // join a chat channel
+    join: function (id) {
+
+    },
+
+    // send text to the current channel
+    send: function (text) {
+
+    },
 
     initEvents: function () {
         // detect enter key
@@ -11,9 +62,43 @@ var chat = {
                 var searchVal = $('#search-browser').val();
             }
         });
+
+        $(document).on('click', '#channel-chat a', function () {
+            var id = $(this).data().id;
+            chat.join(id);
+        });
     }
 };
 
-chat.initEvents();
+// the current user
+var user = {
+    name: null,
+    mutedMicrophone: false,
+    mutedSpeaker: false,
+
+    // toggle a microphone mute
+    muteMicrophone: function () {
+
+    },
+
+    // toggle a speaker mute
+    muteSpeaker: function () {
+
+    }
+};
+
+// users in the server
+var users = {
+    list: [],
+
+    message: function (user, text) {
+        chat.join(user);
+        chat.message(text);
+    }
+};
+
+voice .initEvents();
+chat  .initEvents();
+server.initEvents();
 
 });
