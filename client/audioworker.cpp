b5692bc77197f598f98d1b25ea99665d4362bb81
Charlie Waters <cawiii@me.com>
2016-02-11 01:00:17 -0500

    figuring out opus

--- client/audioworker.cpp
+++ client/audioworker.cpp
@@ -28,15 +28,32 @@ void AudioWorker::run(){
     //outputbuffer->open(QIODevice::ReadWrite | QIODevice::Unbuffered);
 
     int error;
+    // Opus encoder
     encode = opus_encoder_create(client->audioInput->format().sampleRate(), client->audioInput->format().channelCount(), OPUS_APPLICATION_VOIP, &error);
+    if(error < 0){
+        qCritical() << "Faied to create opus encoder: " << opus_strerror(error);
+        return;
+    }
+    error = opus_encoder_ctl(encode, OPUS_SET_BITRATE(OPUS_AUTO));
+    if(error < 0){
+        qCritical() << "Faied to set opus bitrate: " << opus_strerror(error);
+        return;
+    }
+
+    // Opus decoder
+    decode = opus_decoder_create(client->audioInput->format().sampleRate(), client->audioInput->format().channelCount(), &error);
+    if(error < 0){
+        qCritical() << "Faied to create opus decoder: " << opus_strerror(error);
+        return;
+    }
 
     client->audioDeviceMutex.lock();
 
     qInfo() << "Input Devive State:" << client->audioInput->state();
     input = client->audioInput->start();
+    connect(input, SIGNAL(readyRead()), this, SLOT(inputRead()));
     //client->audioInput->setNotifyInterval(NOTIFY_MS);
     //connect(client->audioInput, SIGNAL(notify()), this, SLOT(inputNotified()));
-    connect(input, SIGNAL(readyRead()), this, SLOT(inputRead()));
     qInfo() << "Input Devive State:" << client->audioInput->state();
 
     qInfo() << "Output Devive State:" << client->audioOutput->state();
@@ -52,9 +69,15 @@ void AudioWorker::run(){
 void AudioWorker::inputRead(){
     client->audioDeviceMutex.lock();
     qint64 len = client->audioInput->bytesReady();
+    // Limit read to buffer size
     if(len > INPUT_BUFFER) len = INPUT_BUFFER;
     qInfo() << "Read " << len;
+    // Read PCM data
     input->read(loopbuffer->data(), len);
+
+    QByteArray odata(INPUT_BUFFER, 0);
+    opus_int32 olen = opus_encode(encode, (opus_int16 *)loopbuffer->data(), 0, (unsigned char *)odata.data(), INPUT_BUFFER);
+
     output->write(loopbuffer->data());
     client->audioDeviceMutex.unlock();
 }
